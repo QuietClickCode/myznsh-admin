778a9f37feccae6fdd85859d33b084412d181bd3
zjjlovetj <2572417548@qq.com>
2019-10-06 02:39:01 +0800

    准备创建分支

--- src/main/java/com/zjj/controller/AdminController.java
+++ src/main/java/com/zjj/controller/AdminController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  **/
 @Controller
 public class AdminController {
-
-
     @RequestMapping("/{parameter}")
     public String parameter(@PathVariable("parameter") String parameter) {
         return parameter;
